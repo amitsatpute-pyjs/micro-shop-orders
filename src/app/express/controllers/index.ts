7326f581d4a4f0037590e28c403c43c21caee1f5
amitsatpute-gslab <amitkumar.satpute@gslab.com>
2023-03-07 20:41:49 +0530

    CRUD service api's added

--- src/app/express/controllers/index.ts
+++ src/app/express/controllers/index.ts
@@ -1,11 +1,21 @@
-import { listOrders } from "../../../core/services/orders";
+import {
+  addOrder,
+  deleteOrder,
+  listOrders,
+  orderById,
+  updateOrder,
+} from "../../../core/services/orders";
 import { NextFunction, Response, Request } from "express";
 
 export async function addNewOrder(
   req: Request,
   res: Response,
   next: NextFunction
-): Promise<void> {}
+): Promise<void> {
+  const body = { ...req.body, productIds: JSON.stringify(req.body.productIds) };
+  const data = await addOrder(body);
+  res.json(data);
+}
 
 export async function getOrdersList(
   req: Request,
@@ -13,6 +23,33 @@ export async function getOrdersList(
   next: NextFunction
 ): Promise<void> {
   const data = await listOrders();
-  console.log(data);
   res.json(data);
+}
+
+export async function getOrderById(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  const order = await orderById(req.params.id);
+  res.json(order);
+}
+
+export async function deleteOrderById(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  const order = await deleteOrder(req.params.id);
+  res.json(order);
+}
+
+export async function updateOrderById(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  const body = { ...req.body, productIds: JSON.stringify(req.body.productIds) };
+  const order = await updateOrder(req.params.id, body);
+  res.json(order);
 }
--- src/app/express/routes/orders.ts
+++ src/app/express/routes/orders.ts
@@ -1,5 +1,19 @@
 import { Router } from "express";
-import { getOrdersList } from "../controllers";
+import {
+  addNewOrder,
+  getOrderById,
+  getOrdersList,
+  deleteOrderById,
+  updateOrderById,
+} from "../controllers";
+
 const router = Router();
+
 router.get("/orders", getOrdersList);
+router.post("/addorder", addNewOrder);
+router.get("/order/:id", getOrderById);
+router.delete("/deleteorder/:id", deleteOrderById);
+router.put("/updateorder/:id", updateOrderById);
+// router.put("/detailorder/:id", getDetailOrder);
+
 export default router;
