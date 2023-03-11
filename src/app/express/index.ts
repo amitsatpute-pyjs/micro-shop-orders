2a4ccc7d74b0e6d86e35d6558454a4a2d7f1ad69
amitsatpute-gslab <amitkumar.satpute@gslab.com>
2023-03-11 17:34:53 +0530

    cors added

--- src/app/express/index.ts
+++ src/app/express/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import { router } from "./routes";
 
@@ -8,6 +9,7 @@ const port = process.env.PORT||8081
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use(router);
 
 app.listen(port, () =>
